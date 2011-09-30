1bfdc001ce2800fe344d3e48a25e7c95def2acbe
Ivan Kasatenko <sky.31338@gmail.com>
2011-09-30 18:24:45 +0400

    Added +x to the files

--- make.sh
+++ make.sh
@@ -5,9 +5,9 @@ echo "Making an installation script..."
 TGZ_FILE=/tmp/uniq_scripts_install.tgz
 BASE64_FILE=/tmp/uniq_scripts_install.tgz.base64
 
-tar cfz $TGZ_FILE uniq*
+tar cvfz $TGZ_FILE uniq-add-project uniq-add-project-sued uniq-del-project uniq-login
 base64 -i $TGZ_FILE -o $BASE64_FILE
-rm -f $TGZ_FILE
+# rm -f $TGZ_FILE
 
 cat install-base-1 $BASE64_FILE install-base-2 > install.sh
 chmod a+x install.sh
